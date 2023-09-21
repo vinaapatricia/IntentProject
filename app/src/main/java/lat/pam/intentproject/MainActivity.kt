016f5257bc282f07cef2ae9150c412aa6bf6b1bc
vinaapatricia <vinaapatricia@gmail.com>
2023-09-21 22:05:11 +0700

    Commit

--- app/src/main/java/lat/pam/intentproject/MainActivity.kt
+++ app/src/main/java/lat/pam/intentproject/MainActivity.kt
@@ -4,16 +4,16 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-    private val LOG_TAG = MainActivity::class.java.simpleName
-    val EXTRA_MESSAGE = "com.example.android.twoactivities.extra.MESSAGE"
-    val EXTRA_REPLY = "com.example.android.twoactivities.extra.REPLY"
-
-    val TEXT_REQUEST = 1
+    private val LOG_TAG = "MainActivity"
+    private val TEXT_REQUEST = 1
+    private val EXTRA_MESSAGE = "com.example.android.twoactivities.extra.MESSAGE"
+    private val EXTRA_REPLY = "com.example.android.twoactivities.extra.REPLY"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,28 +22,27 @@ class MainActivity : AppCompatActivity() {
         val mMessageEditText = findViewById<EditText>(R.id.editText_main)
         val btnKirim = findViewById<Button>(R.id.button_main)
 
-        btnKirim?.setOnClickListener({
+        btnKirim?.setOnClickListener {
             Log.d(LOG_TAG, "Button clicked!")
             val intent = Intent(this, SecondActivity::class.java)
             val message = mMessageEditText?.text.toString()
-             Log.d(LOG_TAG, message)
+            Log.d(LOG_TAG, message)
             intent.putExtra(EXTRA_MESSAGE, message)
             startActivityForResult(intent, TEXT_REQUEST)
-        })
+        }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode === TEXT_REQUEST) {
-            if (resultCode === RESULT_OK) {
+        if (requestCode == TEXT_REQUEST) {
+            if (resultCode == RESULT_OK) {
                 val mReplyHeadTextView = findViewById<TextView>(R.id.text_header_reply)
                 val mReplyTextView = findViewById<TextView>(R.id.text_message_reply)
                 val reply = data?.getStringExtra(EXTRA_REPLY)
-                mReplyHeadTextView.visibility
+                mReplyHeadTextView.visibility = View.VISIBLE
                 mReplyTextView.text = reply
-                mReplyTextView.visibility
+                mReplyTextView.visibility = View.VISIBLE
             }
         }
-
     }
 }
